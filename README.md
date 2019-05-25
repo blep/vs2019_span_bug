d01f5c61bbbb7be426ec17857360c0fce3bda012
Baptiste Lepilleur <baptiste.lepilleur@gmail.com>
2019-05-25 14:34:45 +0200

    added a minimalist reproduction for failing test\span_test.cpp with VS 2019. Initial investigation hints that it may be caused by the new memmove intrinsic.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# vs2019_span_bug
-Bug reproduction for a failing test I have with Visual Studio 2019 in Release build.
+# Minimal code for reproduction of failing span_test with Visual Studio 2019
+
+A small bug reproduction of a unit test that fail when executed with Visual Studio 2019 (as of 2019-05-25). Works fine in debug build or VS 2018.
+
+I suspect that the issue is caused by the new memmove intrinsic (could also be some Undefined Behavior I'm not seeing):
+- test pass if memmove is called via a function pointer
+- test pass if size passed to memmove is large (change array size to 50 and number of items to move accordingly)
+
+Baptiste Lepilleur.
---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+#if 0
+// The issue disappear if I force memmove to be a function pointer
+// (which I guess disable intrinsic optimization)
+void *(*memmoveFnPtr)(void*, const void*, size_t) = &memmove;
+#define memmove memmoveFnPtr
+#endif
+
+template<size_t size>
+static void dumpArray(int(&arr)[size], const char *context)
+{
+    printf("%s", context);
+    for (int v : arr)
+    {
+        printf("%d, ", v);
+    }
+    printf("\n");
+}
+
+
+int main()
+{
+    int valuesC[5] = { 10, 20, 30, 40, 50 };
+    const int expectedC[5] = { 10, 20, 20, 30, 40 };
+
+    dumpArray(valuesC, "Before: ");
+
+    memmove(valuesC + 2, valuesC + 1, 3 * sizeof(int));
+
+    dumpArray(valuesC, "After:  ");
+    for (int index = 0; index < 5; ++index)
+    {
+        if (expectedC[index] != valuesC[index])
+        {
+            printf("\nFAILED!\n");
+            abort();
+        }
+    }
+
+    printf("\nOK\n");
+
+    return 0;
+}
